6866fdc2e4b668df88381cac7f4221977f5153ee
Alsav87 <88552900+Alsav87@users.noreply.github.com>
2021-11-27 18:51:09 +0700

    "master"

--- Task4/script.js
+++
@@ -1,6 +0,0 @@
-function changelink(){
-  const link = document.querySelector(".link");
-  const pr = prompt ('Измените текст ссылки');
-  EventTarget.addEventListener('click'),() => {TextContent 
-  };
-};
